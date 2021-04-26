9979af2b678756ab6902271f6527ee655abff863
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-04-26 16:49:31 -0500

    feat: update to ControlFlowExercise

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class ConsoleExercises {
     public static void main(String[] args){
-        
+        //#1 //Write some Java code to output the following: 'The value of pi is approximately 3.14.'
+//        double pi = 3.14159;
+//        System.out.format("The value of pi is approximately %.2f.\n", pi);
+//
+//        //#1 //Exploring the Scanner class.
+        Scanner sc = new Scanner(System.in);
+//        //
+//        System.out.println("Hi, Please enter a number!");
+//        int userNum = sc.nextInt();
+//        System.out.println("The number you entered is " + userNum + ".");
+
+        //#2
+//        System.out.print("Please enter three words!\n");
+//        String word1 = sc.next();
+//        String word2 = sc.next();
+//        String word3 = sc.next();
+//
+//        System.out.printf("You entered: \n %s\n %s\n %s.\n", word1, word2, word3);
+
+        //#3
+//        System.out.print("Please enter a sentence.\n");
+//        String userSentence = sc.nextLine();
+//
+//        System.out.printf("Lets print your sentence:\n %s", userSentence);
+
+        //Calculate the perimeter and area of Codeup's classrooms.
+
+        //#1
+        System.out.print("What is the length and width of the codeup classroom?");
+
     }
 }
---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,19 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+
+        //#1 Loop Basics
+
+        //a. While
+        int i = 5;
+        while(i <= 15) {
+            System.out.println("i is currently at: " + i);
+            i++;
+        }
+
+        //b. Do-While
+        int i = 0;
+        do () {
+
+        }
+    }
+}
