b7fd600c13738dd8bad7e13ea095aa8e36e5427d
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-04-27 09:06:31 -0500

    Feat: completed console exercises walkthrough

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ConsoleExercises {
@@ -30,7 +31,37 @@ public class ConsoleExercises {
         //Calculate the perimeter and area of Codeup's classrooms.
 
         //#1
-        System.out.print("What is the length and width of the codeup classroom?");
+//        System.out.print("What is the length of the codeup classroom.\n");
+//        int length = Integer.parseInt(sc.nextLine());
+//
+//        System.out.println("Please enter the width of the classroom.\n");
+//        int width = Integer.parseInt(sc.nextLine());
+//
+//        int area = length * width;
+//        int perimeter = (length * 2) + (width * 2);
+//
+//        System.out.printf("The area is: %d\n", area);
+//        System.out.printf("The perimeter is: %d\n", perimeter);
 
+        //Bonus:
+        sc.useDelimiter("\n");
+        System.out.print("What is the length of the codeup classroom.\n");
+        double length = sc.nextDouble();
+
+//        System.out.println("Please enter the width of the classroom.\n");
+        double width = sc.nextDouble();
+
+//        System.out.println("Please enter the height of the classroom.\n");
+        double height = sc.nextDouble();
+
+        System.out.printf("Length = %f, width = %f, height = %f\n", length, width, height);
+
+        double area = length * width;
+        double perimeter = (length * 2) + (width * 2);
+        double volume = area * height;
+
+        System.out.printf("The area is: %f\n", area);
+        System.out.printf("The perimeter is: %f\n", perimeter);
+        System.out.printf("The volume is: %f\n", volume);
     }
 }
--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -4,16 +4,58 @@ public class ControlFlowExercises {
         //#1 Loop Basics
 
         //a. While
-        int i = 5;
-        while(i <= 15) {
-            System.out.println("i is currently at: " + i);
-            i++;
-        }
+//        int i = 5;
+//        while(i <= 15) {
+//            System.out.println("i is currently at: " + i);
+//            i++;
+//        }
 
         //b. Do-While
-        int i = 0;
-        do () {
+        //count to 100 by 2
+//        int i = 0;
+//        do {
+//            System.out.println(i);
+//            i += 2;
+//        } while (i <= 100);
 
+        //count backwards by 5's from 100 to -10
+//        int i = 100;
+//        do {
+//            System.out.println(i);
+//            i -= 5;
+//        } while (i >= -10);
+
+        // Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
+        //
+        // 2
+        // 4
+        // 16
+        // 256
+        // 65536
+
+//        long i = 2;
+//        do {
+//            System.out.println(i);
+//            i *= i;
+//        } while (i <= 1000000);
+
+        //Refactor using a for loop
+        //Count to 100 by two's
+//        for (int i = 0; i <= 100; i++) {
+//            if (i % 2 == 0) {
+//                System.out.println(i);
+//            }
+//        }
+
+        //FizzBuzz
+        //#1 1 to 100
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+
+            //#2 multiples of three print "Fizz"
+            if (i % 3 == 0) {
+                System.out.println(Fizz);
+            }
         }
     }
 }
