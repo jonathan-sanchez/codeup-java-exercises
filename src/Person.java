9aa7d8db1ab2c71339d018634864c57f21b1a218
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-05-03 10:50:24 -0500

    feat: updated

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,15 @@
 public class Person {
     private String name;
+
+    public String getName(String name){
+        System.out.println();
+    }
+
+    public void setName(String name){
+
+    }
+
+    public void sayHello(){
+
+    }
 }
