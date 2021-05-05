2b3cd98201e592b2a57ce4265d5802facb04d003
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-05-05 09:11:16 -0500

    feat: morning push up

--- src/Shapes/Rectangle.java
+++ src/Shapes/Rectangle.java
@@ -1,4 +1,10 @@
 package Shapes;
 
 public class Rectangle {
+    protected double length;
+    protected double width;
+
+    public Rectangle(double length, double width){
+        return length + width;
+   }
 }
