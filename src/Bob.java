31ef836e2e370b4e0e4372bbe06ea60ce46288bb
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-05-06 09:04:41 -0500

    feat: morning push up

--- src/Bob.java
+++ src/Bob.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Bob {
     public static void main(String[] args) {
+        System.out.println("Hello! I am Bob!");
 
+        Scanner Bob = new Scanner(System.in);
+
+        String userInput = Bob.nextLine();
+
+        System.out.println(userInput);
     }
+
 }
