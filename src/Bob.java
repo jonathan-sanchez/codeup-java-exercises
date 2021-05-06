31537832b9ab15fb1d6d7f5703f4c22f49ebd931
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-05-06 13:22:27 -0500

    feat: just need to loop the conversation with bob

--- src/Bob.java
+++ src/Bob.java
@@ -2,31 +2,32 @@ import java.util.Scanner;
 
 public class Bob {
     public static void main(String[] args) {
-        System.out.println("Hello! I am Bob!");
+        System.out.println("Hello! I am Bob!\n");
+        System.out.println("Bob likes Java!\n");
 
-
-
-
+        grumpyOlBob();
     }
 
     public static void grumpyOlBob() {
-        boolean stillTalking;
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Ask bob a question!");
         String userInput = sc.nextLine();
 
-        System.out.println("Ask bob a question!");
-
-        switch(userInput) {
-            case (userInput.endsWith("?")):
-                System.out.println("Sure");
-                break;
+        if (userInput.endsWith("?")) {
+            System.out.println("Sure");
+        } else if (userInput.endsWith("!")) {
+            System.out.println("Whoa, chill out!");
+        } else if (userInput == "") {
+            System.out.println("Fine, be that way!");
+        } else {
+            System.out.println("Whatever.");
         }
 
     }
 }
 
-//For reference trying it out with a switch statement
+//For reference
 
 //    public static void main(String[] args){
 //        talkToBob();
