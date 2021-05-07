dc88cc0165950826bc72ef99deacf6feefb2fcd4
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-05-07 15:53:58 -0500

    feat: completed section 1

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -7,7 +7,22 @@ import java.util.Map;
 public class Student {
 
     public static void main(String[] args) {
-        Map<String, ArrayList<Integer>> student = new HashMap<>();
+
+        Student student1 = new Student("Jonathan");
+        System.out.println("student1.getName() = " + student1.getName());
+        student1.addGrade(98);
+        student1.addGrade(90);
+        student1.addGrade(85);
+        System.out.println("student1.grades = " + student1.grades);
+        System.out.println("student1.getGradeAverage() = " + student1.getGradeAverage());
+
+        Student student2 = new Student("Kapena");
+        System.out.println("student2.getName() = " + student2.getName());
+        student2.addGrade(87);
+        student2.addGrade(90);
+        student2.addGrade(96);
+        System.out.println("student2.grades = " + student2.grades);
+        System.out.println("student2.getGradeAverage() = " + student2.getGradeAverage());
 
 
     }
@@ -16,29 +31,31 @@ public class Student {
     private String name;
 
 
-    public String student() {
-        this.name = "Jonathan";
-        this.grades =new ArrayList<>();
+    public Student(String name) {
+        this.name = name;
+        this.grades = new ArrayList<>();
     }
 
     public String getName() {
     return name;
     }
 
-    public static void setName(String name){
-
-    }
-
-    public int getGrades() {
-         return this.grades = new ArrayList<>() ;
+    public void setName(String name){
+        this.name = name;
+        this.grades = new ArrayList<>();
     }
 
     public void addGrade(int grade){
-
+        grades.add(grade);
     }
 
-    public double getGradeAverage(){
+    int gradeAverage;
+    public int getGradeAverage(){
 
+        for (int i = 0; i < grades.size(); i++) {
+            gradeAverage += grades.get(i);
+        }
+        return gradeAverage / grades.size();
     }
 
 
