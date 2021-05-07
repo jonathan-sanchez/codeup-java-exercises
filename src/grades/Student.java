20bd4af033603025bef546d5a4d25f57fc286117
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-05-07 14:02:39 -0500

    feat: afternoon push up

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -1,6 +1,45 @@
 package grades;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Student {
+
+    public static void main(String[] args) {
+        Map<String, ArrayList<Integer>> student = new HashMap<>();
+
+
+    }
+
+    private ArrayList<Integer> grades;
     private String name;
-    private double grades;
+
+
+    public String student() {
+        this.name = "Jonathan";
+        this.grades =new ArrayList<>();
+    }
+
+    public String getName() {
+    return name;
+    }
+
+    public static void setName(String name){
+
+    }
+
+    public int getGrades() {
+         return this.grades = new ArrayList<>() ;
+    }
+
+    public void addGrade(int grade){
+
+    }
+
+    public double getGradeAverage(){
+
+    }
+
+
 }
